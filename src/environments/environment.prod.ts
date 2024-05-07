f6dc93fc1a5227a2325823b512c133db02ca4437
ppa-opash <ppa.opash@gmail.com>
2024-05-07 12:16:02 +0530

    Change url and images

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
   hmr: false,
-  serverUrl: 'https://api.organic.dating/api/v1/',
+  serverUrl: 'https://api.gamble.social/api/v1/',
   serverUrl2: 'http://localhost:8080/api/v1/',
-  socketUrl: 'https://api.organic.dating/',
+  socketUrl: 'https://api.gamble.social/',
   socketUrl2: 'http://localhost:8080/',
   imgUrl: 'root/storage/uploads',
 
